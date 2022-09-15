942a821478d5262a861dc7b6ffea32eb8e4f9b71
Jane Sandberg <js7389@princeton.edu>
2022-09-15 13:34:50 -0700

    i101: Don't attempt to create a duplicate user in cas login
    Co-authored-by: Carolyn Cole <carolyncole@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   end
 
   def self.from_cas(access_token)
-    user = User.find_by(provider: access_token.provider, uid: access_token.uid)
+    user = User.find_by(uid: access_token.uid)
     user = User.create(provider: access_token.provider, uid: access_token.uid, admin: false) if user.blank?
     user
   end
