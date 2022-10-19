9f16000156c8ca3abb21226d490f118be4d28ed3
Ryan Laddusaw <rladdusaw@princeton.edu>
2022-10-19 08:51:53 -0500

    Updated labels and some styles

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -14,4 +14,16 @@ class ApplicationRecord < ActiveRecord::Base
   def prepare_choices_for_lux(choices)
     choices.map { |val| { label: val, value: val } }
   end
+
+  def prepare_size_choices_for_lux(choices)
+    choices.map { |val| { label: "#{val}-foot", value: val } }
+  end
+
+  def prepare_semester_choices_for_lux(choices)
+    choices.map do | val |
+      puts val
+      label = (val == "Fall") ? "Fall & Spring" : "Spring Only"
+      { label: label, value: val }
+    end
+  end
 end
