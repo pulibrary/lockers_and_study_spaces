dabd453dde6df1b1db0d5dd1d736d03007b0d048
Jane Sandberg <js7389@princeton.edu>
2022-10-25 15:44:54 -0700

    Lockers index only display lockers at the admin's library

--- spec/controllers/lockers_controller_spec.rb
+++ spec/controllers/lockers_controller_spec.rb
@@ -4,11 +4,27 @@ require 'rails_helper'
 
 RSpec.describe LockersController do
   render_views
-  let(:building) { FactoryBot.create :building, name: 'Lewis Library' }
-  let(:user) { FactoryBot.create :user, :admin, building: building }
+  let(:firestone) { FactoryBot.create :building, name: 'Firestone Library' }
+  let(:lewis) { FactoryBot.create :building, name: 'Lewis Library' }
+  let(:user) { FactoryBot.create :user, :admin, building: lewis }
+  let(:firestone_locker) { FactoryBot.create :locker, building: firestone }
+  let(:lewis_locker) { FactoryBot.create :locker, building: lewis }
 
   before do
     sign_in user
+  end
+
+  describe '#index' do
+    before do
+      firestone_locker
+      lewis_locker
+    end
+
+    it "only includes lockers for the admin's building" do
+      controller.index
+      expect(controller.instance_variable_get('@lockers')).to include(lewis_locker)
+      expect(controller.instance_variable_get('@lockers')).not_to include(firestone_locker)
+    end
   end
 
   context 'when Lewis staff features are turned on' do
