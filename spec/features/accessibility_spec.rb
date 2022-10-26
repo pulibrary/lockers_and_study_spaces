70d1c11342af265a712294ef67f19d788b3dc713
Jane Sandberg <js7389@princeton.edu>
2022-10-26 14:55:51 -0700

    i86: <html> element should have a lang attribute

---
+++ spec/features/accessibility_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'axe-rspec'
+
+describe 'accessibility', type: :feature, js: true do
+  context 'when visiting the home page' do
+    before do
+      visit '/'
+    end
+
+    it 'complies with wcag' do
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa)
+        .skipping(:'duplicate-id-aria', :list)
+    end
+  end
+end
