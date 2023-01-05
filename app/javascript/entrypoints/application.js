eebee48ab29529418882c5af5e8e27ad7b78afb9
Jane Sandberg <js7389@princeton.edu>
2023-01-05 09:39:37 -0800

    Import styles before vue and lux
    Co-authored-by: Shaun Ellis <sdellis@users.noreply.github.com>
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,12 +1,12 @@
-import Vue from "vue/dist/vue.esm"
-import system from "lux-design-system"
 import "lux-design-system/dist/system/system.css"
 import "lux-design-system/dist/system/tokens/tokens.scss"
+import Vue from "vue/dist/vue.esm"
+import system from "lux-design-system"
 
 Vue.use(system)
 
 // create the LUX app and mount it to wrappers with class="lux"
-document.addEventListener("load", () => {
+document.addEventListener("DOMContentLoaded", () => {
   var elements = document.getElementsByClassName("lux")
   for (var i = 0; i < elements.length; i++) {
     new Vue({
