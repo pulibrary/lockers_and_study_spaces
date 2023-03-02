97dfdfc53a190aedfe8a62cfad0c0e4c680d5f07
regineheberlein <regineheberlein@users.noreply.github.com>
2023-03-02 15:09:56 -0500

    show Lewis Library on locker assignment card (#206)
    * show Lewis Library on locker assignment card
    
    Co-authored-by: Max Kadel <mkadel@princeton.edu>

---
+++ spec/views/locker_assignments/card.html.erb_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'locker_assignments/card' do
+  let(:building_one) { FactoryBot.create(:building, id: 1) }
+  let(:building_two) { FactoryBot.create(:building, name: 'Lewis Library', id: 2) }
+
+  before do
+    @locker_assignment = assign(:locker_assignment, FactoryBot.create(:locker_assignment,
+                                                                      locker_application:,
+                                                                      locker:))
+  end
+
+  context 'with lewis_patrons on' do
+    before do
+      allow(Flipflop).to receive(:lewis_patrons?).and_return(true)
+      allow(Flipflop).to receive(:lewis_staff?).and_return(true)
+    end
+
+    context 'with firestone_locker_assignment' do
+      let(:locker) { FactoryBot.create(:locker, building: building_one) }
+      let(:locker_application) { FactoryBot.create(:locker_application, building: building_one) }
+
+      it 'shows a print view of the locker assignment' do
+        render
+        expect(rendered).to match(/Firestone Library/)
+      end
+    end
+
+    context 'with lewis_locker_assignment' do
+      let(:locker) { FactoryBot.create(:locker, building: building_two) }
+      let(:locker_application) { FactoryBot.create(:locker_application, building: building_two) }
+
+      it 'shows a print view of the locker assignment' do
+        render
+        expect(rendered).to match(/Lewis Library/)
+      end
+    end
+  end
+end
