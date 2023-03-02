335142cc3c0f6a2260b177762f726b2ffcfe2e78
regineheberlein <regineheberlein@gmail.com>
2023-03-02 14:57:39 -0500

    rubocopped; one error remaining

--- spec/views/locker_assignments/card.html.erb_spec.rb
+++ spec/views/locker_assignments/card.html.erb_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe 'locker_assignments/card' do
   let(:building_one) { FactoryBot.create(:building, id: 1) }
   let(:building_two) { FactoryBot.create(:building, name: 'Lewis Library', id: 2) }
+
   before do
     @locker_assignment = assign(:locker_assignment, FactoryBot.create(:locker_assignment,
                                                                       locker_application:,
@@ -16,17 +17,21 @@ RSpec.describe 'locker_assignments/card' do
       allow(Flipflop).to receive(:lewis_patrons?).and_return(true)
       allow(Flipflop).to receive(:lewis_staff?).and_return(true)
     end
+
     context 'with firestone_locker_assignment' do
       let(:locker) { FactoryBot.create(:locker, building: building_one) }
       let(:locker_application) { FactoryBot.create(:locker_application, building: building_one) }
+
       it 'shows a print view of the locker assignment' do
         render
         expect(rendered).to match(/Firestone Library/)
       end
     end
+
     context 'with lewis_locker_assignment' do
       let(:locker) { FactoryBot.create(:locker, building: building_two) }
       let(:locker_application) { FactoryBot.create(:locker_application, building: building_two) }
+
       it 'shows a print view of the locker assignment' do
         render
         expect(rendered).to match(/Lewis Library/)
