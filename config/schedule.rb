576ab87771c9637617796dcdbce55239e18b5811
Jane Sandberg <js7389@princeton.edu>
2023-09-27 07:39:22 -0700

    i248: only run cron on prod2
    Based on https://github.com/pulibrary/lib_jobs/pull/640

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@
 set :output, '/opt/lockers_and_study_spaces/shared/tmp/cron_log.log'
 env :PATH, ENV.fetch('PATH', nil)
 
-every 1.day, at: '6:00 am' do
+every 1.day, at: '6:00 am', roles: [:cron_prod2] do
   rake 'email:scheduled:send:firestone'
   rake 'email:scheduled:send:lewis'
 end
