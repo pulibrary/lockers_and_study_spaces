00935a3dad544231826727fbf897713ae04f2e7f
Ryan Laddusaw <rladdusaw@princeton.edu>
2024-08-07 16:21:15 -0400

    Only allow 6' locker on A floor

---
+++ app/javascript/components/locker_size_filter.js
@@ -0,0 +1,29 @@
+export default class LockerSizeFilter {
+  constructor() {
+    this.preferredFloor = document.querySelector(
+      "#locker_application_preferred_general_area"
+    );
+    this.preferredSize = document.querySelector(
+      "#locker_application_preferred_size"
+    );
+    this.library = document
+      .querySelector("#locker-form-header a")
+      .innerHTML.split(" ")[0];
+    this.#addListener();
+  }
+
+  #addListener() {
+    this.preferredFloor.addEventListener("change", () => {
+      if (this.library === "Firestone") {
+        if (this.preferredFloor.value === "A floor") {
+          this.preferredSize.options.length = 0;
+          this.preferredSize[0] = new Option("6-foot", 6);
+        } else {
+          this.preferredSize.options.length = 0;
+          this.preferredSize[0] = new Option("4-foot", 4);
+          this.preferredSize[1] = new Option("6-foot", 6);
+        }
+      }
+    });
+  }
+}
