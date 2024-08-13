9c091acaa8bc709fcc2fc36bc5a13e542391c761
Christina Chortaria <actspatial@gmail.com>
2024-08-13 17:12:26 -0400

    Update study room violation spec Fix fixture specs Update lux-data-table tag.

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import { createApp } from 'vue';
 import lux from 'lux-design-system';
-// import Rails from '@rails/ujs'; // we need to import Rails so vite adds it to the module
-// import CopyToClipboard from '../components/copy_to_clipboard';
-// import LockerSizeFilter from '../components/locker_size_filter';
+import Rails from '@rails/ujs'; // we need to import Rails so vite adds it to the module
+import CopyToClipboard from '../components/copy_to_clipboard';
+import LockerSizeFilter from '../components/locker_size_filter';
 import 'lux-design-system/dist/style.scss';
 import 'lux-design-system/dist/style.css';
 
