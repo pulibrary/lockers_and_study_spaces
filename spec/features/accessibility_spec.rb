c5c2cc6dfbd372082fdbc21e9c00ff11404460c3
Vivian Ha <vh8154@princeton.edu>
2024-11-13 11:01:29 -0500

    Rebasing to main

--- spec/features/accessibility_spec.rb
+++ spec/features/accessibility_spec.rb
@@ -4,6 +4,13 @@ require 'rails_helper'
 require 'axe-rspec'
 
 describe 'accessibility', :js do
+  let(:admin) { FactoryBot.create(:user, admin: true) }
+  let(:firestone) { FactoryBot.create(:building, name: 'Firestone Library', id: 1) }
+
+  before do
+    firestone
+  end
+
   context 'when visiting the home page' do
     before do
       visit '/'
@@ -13,6 +20,22 @@ describe 'accessibility', :js do
       expect(page).to be_axe_clean
         .according_to(:wcag2a, :wcag2aa)
         .skipping(:list)
+    end
+  end
+
+  context 'when creating new locker application' do
+    before do
+      sign_in admin
+    end
+
+    let(:locker_application) do
+      FactoryBot.create(:locker_application, complete: true, accessibility_needs: ['Keyed entry (rather than combination)', 'Another need'])
+    end
+
+    it 'complies with wcag' do
+      visit edit_locker_application_path(id: locker_application.id)
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa)
     end
   end
 
